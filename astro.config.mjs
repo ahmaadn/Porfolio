109539abbb617edbc6900addb0550ec326bbb8ee
Ahmad Nur <62164035+ahmaadn@users.noreply.github.com>
2023-09-03 10:36:34 +0700

    deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig, squooshImageService } from "astro/config";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	image: {
-		service: squooshImageService(),
-	},
+  integrations: [tailwind()],
+  image: {
+    service: squooshImageService()
+  },
+  output: "server",
+  adapter: netlify()
 });
