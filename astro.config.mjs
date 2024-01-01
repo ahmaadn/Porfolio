ee3e4f2993fb54b328d461b898c5c146c3942818
Ahmad Nur <62164035+ahmaadn@users.noreply.github.com>
2024-01-01 08:32:34 +0700

    feet : add dependencie astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
+  	integrations: [
 		tailwind({
-			applyBaseStyles: false,
-		}),
-		react(),
+   		applyBaseStyles: false,
+  	}),
+  	react(), compress()
 	],
 });
