6c2004a09b812a1114362d2f2ad8cb9b0711cba9
Ahmad Nur <62164035+ahmaadn@users.noreply.github.com>
2024-01-05 21:33:13 +0700

    feet: resolve dependencies

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -1,4 +1,3 @@
-import { buttonVariants } from "./button";
 import { cn } from "@/lib/utils";
 import * as SheetPrimitive from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
@@ -11,10 +10,7 @@ const SheetTrigger = SheetPrimitive.Trigger;
 
 const SheetClose = SheetPrimitive.Close;
 
-const SheetPortal = ({ className, ...props }: SheetPrimitive.DialogPortalProps) => (
-	<SheetPrimitive.Portal className={cn(className)} {...props} />
-);
-SheetPortal.displayName = SheetPrimitive.Portal.displayName;
+const SheetPortal = SheetPrimitive.Portal;
 
 const SheetOverlay = React.forwardRef<
 	React.ElementRef<typeof SheetPrimitive.Overlay>,
@@ -22,7 +18,7 @@ const SheetOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
 	<SheetPrimitive.Overlay
 		className={cn(
-			"fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+			"fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
 			className
 		)}
 		{...props}
@@ -64,12 +60,8 @@ const SheetContent = React.forwardRef<
 			className={cn(sheetVariants({ side }), className)}
 			{...props}>
 			{children}
-			<SheetPrimitive.Close
-				className={cn(
-					buttonVariants({ variant: "ghost", size: "icon" }),
-					"absolute right-4 top-4 opacity-70 disabled:pointer-events-none data-[state=open]:bg-secondary"
-				)}>
-				<Cross2Icon className="h-5 w-5" />
+			<SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+				<Cross2Icon className="h-4 w-4" />
 				<span className="sr-only">Close</span>
 			</SheetPrimitive.Close>
 		</SheetPrimitive.Content>
@@ -116,6 +108,8 @@ SheetDescription.displayName = SheetPrimitive.Description.displayName;
 
 export {
 	Sheet,
+	SheetPortal,
+	SheetOverlay,
 	SheetTrigger,
 	SheetClose,
 	SheetContent,
