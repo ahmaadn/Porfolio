e6496d0828d055be0c7bca67cf885ecf94eab2f7
Ahmad Nur <62164035+ahmaadn@users.noreply.github.com>
2024-01-05 21:33:13 +0700

    feet: resolve dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	image: {
+		service: squooshImageService(),
+	},
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
