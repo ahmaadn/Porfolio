3aebb6c7f01d3f9c9c575d46c52d2ba438a6d6bc
Ahmad Nur <ahmadnursahid12345@gmail.com>
2024-07-10 19:59:00 +0700

    v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
-  	integrations: [
-		tailwind({
-   		applyBaseStyles: false,
-  	}),
-  	react(), compress()
-	],
+	integrations: [tailwind({ applyBaseStyles: false })],
 });
---
+++ src/config.js
@@ -0,0 +1,40 @@
+export const SITE = {
+	title: "Ahmaadn",
+	author: "Ahmad nur sahid",
+	mail: "ahmadnur415@gmail.com",
+	position: "Fullstack",
+	description:
+		"Halo, saya Ahmad Nur Sahid, biasa dipanggil Ahmad. Saya lahir pada tanggal 5 Agustus. Hobi utama saya adalah bermain game mobile dan coding. Saat ini, saya merupakan mahasiswa Teknik  Informatika di Institut Teknologi Garut. Minat saya terhadap bahasa pemrograman muncul sejak remaja, dan awalnya saya belajar pemrograman ketika masih di sekolah menengah kejuruan (SMK). Bahasa pertama yang saya pelajari adalah Python.",
+};
+
+export const SOSIAL_LINK = [{ label: "Github", url: "https://github.com/ahmaadn" }];
+
+export const EDUCATION = [
+	{
+		from: "2019",
+		to: "2022",
+		institute: "SMKS AL-Farisi Leles",
+		course: "Teknik Komputer Dan Jaringan",
+		location: "Leles, Garut",
+	},
+	{
+		from: "2022",
+		to: "Now",
+		institute: "Institut Teknologi Garut",
+		course: "Teknik Informatika",
+		location: "Garut, Jawa Barat",
+	},
+];
+
+export const PROJECTS = [
+	{
+		name: "Youtube Downloader",
+		tags: ["python"],
+		url: "https://github.com/ahmaadn/Youtube-Downloader",
+	},
+	{
+		name: "Ecommerce",
+		tags: ["Astro", "Vue", "FastApi"],
+		url: "https://github.com/ahmaadn/ecommerce-astro-vue-fastapi",
+	},
+];
