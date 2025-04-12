8672e1f42ceddbfe0df7cdebfe53ae8bfcdd8a73
Ahmad Nur <ahmadnursahid12345@gmail.com>
2025-04-12 21:03:52 +0700

    refactor:  change tailwind version to 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.ahmaadn.my.id",
-	integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+	integrations: [sitemap()],
 	adapter: vercel({
 		webAnalytics: {
 			enabled: true,
 		},
 	}),
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
